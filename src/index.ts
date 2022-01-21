7bf3f6f5ae1fb2f9b96debf9d03c678a67ec8663
Crespo Wang <crespo.wang@inquisitive.com>
2022-01-21 11:24:07 +1100

    fix frozen bug and more tests

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,44 @@
+
 const charsFactory = "!0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz~";
-export const lo = charsFactory[0];
-export const hi = charsFactory[charsFactory.length - 1];
+export const lowest = charsFactory[0];
+export const highest = charsFactory[charsFactory.length - 1];
 
 export const between = (low: string, high: string): string => {
     if (typeof low !== "string" || typeof high !== "string") {
         throw new Error("High and low must be string");
     }
-    if (high.localeCompare(low) <= 0) {
-        throw new Error("Reversed or equal high and low");
+    if (high.localeCompare(low) === 0) {
+        throw new Error("Equal high and low");
+
     }
-
-
+    let correctedHigh;
+    let correctedLow;
+    if (low == lowest || high == highest) {
+        correctedLow = low;
+        correctedHigh = high;
+    } else {
+        correctedHigh = high.localeCompare(low) < 0 ? low : high;
+        correctedLow = high.localeCompare(low) < 0 ? high : low;
+    }
     let newSortValue = "";
     let i = 0;
 
     while (true) {
-        let lowIndex = charsFactory.indexOf(low[i]);
-        let highIndex = charsFactory.indexOf(high[i]);
+        let lowIndex = charsFactory.indexOf(correctedLow[i]);
+        let highIndex = charsFactory.indexOf(correctedHigh[i]);
 
-        if (lowIndex === -1) lowIndex = 0;
-        if (highIndex === -1) highIndex = charsFactory.length - 1;
+        if (lowIndex == -1) lowIndex = 0;
+        if (highIndex == -1) highIndex = charsFactory.length - 1;
 
         i++;
         const newSortCharIndex = lowIndex + 1 < highIndex
             ? Math.round((lowIndex + highIndex) / 2)
             : lowIndex;
+
         const c = charsFactory[newSortCharIndex];
-
-        newSortValue = `${newSortValue}${c}`;
-
-        if (low.localeCompare(newSortValue) < 0 && newSortValue.localeCompare(high) && c != lo)
+        newSortValue += c;
+        if (correctedLow < newSortValue && newSortValue < correctedHigh && c != lowest)
             return newSortValue;
     }
 }
-export default { lo, hi, between };
+export default { lowest, highest, between };
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,34 +1,34 @@
 
+import { between as oldBetween, hi, lo } from "between";
 import { expect } from "chai";
 import between from "../src/index";
 
 describe("when the high and low are at the boundary", (): void => {
     it("returns the sort value", (): void => {
-        const value = between.between(between.lo, between.hi);
-        console.log(value);
-
-        expect(value.length).is.equal(1);
+        const value = between.between(between.lowest, between.highest);
+        expect(value).is.equal("V");
     });
 });
 
 describe("when the high and low are provided", (): void => {
     it("returns the sort value", (): void => {
         const value = between.between("a", "b");
-        console.log(value);
         expect(value.length).is.equal(2);
     });
 });
 
 describe("when the high and low are the same", (): void => {
     it("throws exception", (): void => {
-        expect(() => { between.between("a", "a") }).to.throw("Reversed or equal high and low");
+        expect(() => { between.between("a", "a") }).to.throw("Equal high and low");
     });
 });
 
 describe("when the high and low are reversed", (): void => {
-    it("throws exception", (): void => {
-        expect(() => { between.between("b", "a") }).to.throw("Reversed or equal high and low");
+    it("returns the sort value", (): void => {
+        const value = between.between("b", "a");
+        expect(value.length).is.equal(2);
     });
+
 });
 
 describe("when the low is undefined", (): void => {
@@ -40,5 +40,44 @@ describe("when the low is undefined", (): void => {
 describe("when the high is undefined", (): void => {
     it("throws exception", (): void => {
         expect(() => { between.between("a", null) }).to.throw("High and low must be string");
+    });
+});
+
+describe("when it's between A and AA", (): void => {
+    it("returns AAB", (): void => {
+        const value = between.between("A", "AA");
+        expect(value).to.eq("A5");
+    });
+});
+describe("when the high and low are reversed", (): void => {
+    it("returns the same value", (): void => {
+        const value = between.between("AA", "A");
+        const reversedValue = between.between("A", "AA");
+        expect(value).to.equal(reversedValue);
+    });
+});
+
+
+describe("compare with the old between", (): void => {
+    it("returns the same value", (): void => {
+        let low = "A";
+        for (let i = 0; i < 1000; i++) {
+            const oldValue = oldBetween(low, hi);
+            const newLow = between.between(low, between.highest);
+            expect(newLow).to.equal(oldValue);
+            low = newLow;
+        }
+
+    });
+
+    it("returns the same value", (): void => {
+        let high = "k";
+        for (let i = 0; i < 1000; i++) {
+            const oldValue = oldBetween(lo, high);
+            const newHigh = between.between(between.lowest, high);
+            expect(newHigh).to.equal(oldValue);
+            high = newHigh;
+        }
+
     });
 });
